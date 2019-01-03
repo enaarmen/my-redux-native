56f8cb3f21e8c7905d8ad77c3ce9f9ae9740a6e4
yves adonias <yves.adonias@epitech.eu>
2019-01-03 12:01:28 +0100

    premiers affichages sans retour. On y arrive à ce tuto.

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './components/signInScreen.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+          <HomeScreen />
       </View>
     );
   }
 }
-
+//<Text>Open up App.js to start working on your app!</Text>
 const styles = StyleSheet.create({
   container: {
     flex: 1,
